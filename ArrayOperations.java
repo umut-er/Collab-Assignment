8c9e4e9f2dd0a2f6e0d8c070eb2b028a883b7f78
Erayisci <114355113+Erayisci@users.noreply.github.com>
2023-02-08 19:10:45 +0300

    Update ArrayOperations.java

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -11,8 +11,39 @@ public class ArrayOperations {
         for(int i = 0; i < n; i++){
             array[i] = rand.nextInt(0, 101);
         }
-
-        in.close();
+        boolean keepAsking = true;
+        while(keepAsking)
+        {
+            System.out.println("Press 1 if you want to find the minimum of the array");
+            System.out.println("Press 2 if you want to find the maximum of the array");
+            System.out.println("Press 3 if you want to display the difference of the average and each of the elements in the array");
+            System.out.println("Press 4 if you want to find the sum of odd indexed and the even indexed numbers");
+            System.out.println("Press 5 if you want to quit");
+            int choice = in.nextInt();
+            in.nextLine();
+            if(choice == 1)
+            {
+                System.out.println(minNumberFinder(array));
+            }
+            else if(choice == 2)
+            {
+                System.out.println(maxNumberFinder(array));
+            }
+            else if(choice == 3)
+            {
+                //method gelcek buraya System.outprintln()
+            }
+            else if(choice == 4)
+            {
+                //aynı şekilde
+            }
+            else
+            {
+                keepAsking = false;
+            }
+        }
+       
+           in.close();
     }
     public static int minNumberFinder(int[] theArray){
         int min = Integer.MAX_VALUE;
@@ -34,3 +65,4 @@ public class ArrayOperations {
     }
    
 }
+
