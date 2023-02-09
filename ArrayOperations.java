54f040b15e6c28202db118e9881464332b255c0a
Umut Utku Ersahince <utku.ersahince-ug@labs.bilkent.edu.tr>
2023-02-09 10:08:49 +0300

    style: minor stylistic changes, for consistency

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -11,64 +11,64 @@ import java.util.Random;
  * @author Kemal Onur Özkan
  * @author Umut Utku ERŞAHİNCE
  */
-public class ArrayOperations {
-    public static void main(String[] args) {
+public class ArrayOperations{
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
 
+        // Create an array of given size consisting of elements in range [0, 100].
         System.out.print("Input the number of elements in the array: ");
         int n = in.nextInt();
         System.out.println();
         int[] array = new int[n];
         for(int i = 0; i < n; i++){
-            array[i] = rand.nextInt(0, 101);
+            array[i] = rand.nextInt(101);
         }
         System.out.println("The array: " + Arrays.toString(array) + "\n");
 
-
         boolean keepAsking = true;
-        while(keepAsking)
-        {
-            System.out.println("--------------------------  Options  --------------------------");
-            System.out.println("1 - display minimum of the array");
-            System.out.println("2 - display maximum of the array");
-            System.out.println("3 - display the difference between the average and the elements");
-            System.out.println("4 - display the sum of the odd indexed items");
-            System.out.println("5 - display the sum of the even indexed items");
-            System.out.println("6 - quit");
-            System.out.println("---------------------------------------------------------------");
+        while(keepAsking){
+            viewMenu();
+            System.out.println();
             System.out.print("Input: ");
             int choice = in.nextInt();
             in.nextLine();
             System.out.println();
-            if(choice == 1)
-            {
+            if(choice == 1){
                 System.out.println("Minimum element in the array is: " + minNumberFinder(array));
             }
-            else if(choice == 2)
-            {
+            else if(choice == 2){
                 System.out.println("Maximum element in the array is: " + maxNumberFinder(array));
             }
-            else if(choice == 3)
-            {
+            else if(choice == 3){
                 System.out.println("The array containing the difference between the element and the average:\n" + displayDiffFromAverage(array));
             }
-            else if(choice == 4)
-            {
+            else if(choice == 4){
                 System.out.println("The sum of elements of odd index: " + oddSum(array));
             }
-            else if(choice == 5)
-            {
+            else if(choice == 5){
                 System.out.println("The sum of elements of even index: " + evenSum(array));
             }
-            else if(choice == 6)
-            {
+            else if(choice == 6){
                 keepAsking = false;
             }
             System.out.println();
-        }
-       
+        }       
         in.close();
+    }
+
+    /**
+     * Prints the string for the menu
+     */
+    public static void viewMenu(){
+        System.out.println("--------------------------  Options  --------------------------");
+        System.out.println("1 - display minimum of the array");
+        System.out.println("2 - display maximum of the array");
+        System.out.println("3 - display the difference between the average and the elements");
+        System.out.println("4 - display the sum of the odd indexed items");
+        System.out.println("5 - display the sum of the even indexed items");
+        System.out.println("6 - quit");
+        System.out.println("---------------------------------------------------------------");  
     }
 
     /**
@@ -108,8 +108,7 @@ public class ArrayOperations {
      */
     public static int oddSum(int[] myArr){
         int sum = 0;
-        for(int i = 1; i < myArr.length; i+=2)
-        {
+        for(int i = 1; i < myArr.length; i+=2){
             sum += myArr[i];
         }    
         return sum;
@@ -122,8 +121,7 @@ public class ArrayOperations {
      */
     public static int evenSum(int[] myArr){
         int sum = 0;
-        for(int i = 0; i < myArr.length; i+=2)
-        {
+        for(int i = 0; i < myArr.length; i+=2){
             sum += myArr[i];
         }    
         return sum;
@@ -136,20 +134,16 @@ public class ArrayOperations {
      */
     public static String displayDiffFromAverage(int[] array){
         double sum=0;
-        for(int i=0; i<array.length; i++)
-        {
+        for(int i=0; i<array.length; i++){
             sum+=array[i];
         }
         double average=sum/array.length;
         String differences="";
-        for(int i=0; i<array.length; i++)
-        {
-            if(i==array.length-1)
-            {
+        for(int i=0; i<array.length; i++){
+            if(i==array.length-1){
                 differences+=array[i]-average;
             }
-            else
-            {
+            else{
                 differences+=(array[i]-average)+", ";
             }
         }
@@ -159,19 +153,15 @@ public class ArrayOperations {
     //in case they want this method to return array type
     /*public static double[] displayDiffFromAverage(int[] array){
         double sum=0;
-        for(int i=0; i<array.length; i++)
-        {
+        for(int i=0; i<array.length; i++){
             sum+=array[i];
         }
         double average=sum/array.length;
         double[] result = new double[array.length];
-        for(int i = 0; i < result.length; i++)
-        {
+        for(int i = 0; i < result.length; i++){
             result[i] = array[i] - average;
         }
-
         return result;
-
     }
     */
 }
